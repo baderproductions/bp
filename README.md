fb2cd6f954636c490f75c48b7afe35a5858314ae
baderproductions <kevin.bader96@gmail.com>
2020-03-12 23:25:23 +0000

    media querries for download button and some resizing

---
+++ README.md
@@ -0,0 +1,16 @@
+# Introduction
+
+I builded this website as a show-off platform to expose all my Web Development projects Frontend, Backend and Full Stack.<br> This particular website is builded with React and is hosted on [Netlify](https://www.netlify.com/) with a **custom domain**.<br>
+Can be seen at https://baderproductions.net/
+
+# Packages used for this website
+
+- [react-helmet](https://www.npmjs.com/package/react-helmet) used for dynamic page title and SEO
+  depending on each page component
+- [react-messenger-customer-chat](https://www.npmjs.com/package/react-messenger-customer-chat) - Facebook Messenger's Customer Chat API
+- [redux](https://www.npmjs.com/package/redux) for state management (implemented but unused for time being)
+- [node-sass](https://www.npmjs.com/package/node-sass) for live hot-reloading SCSS directly from one file without CSS to SCSS conversion
+
+# Technologies used
+
+![](http://i.epvpimg.com/lxJHaab.png) ![](http://i.epvpimg.com/161Wbab.png) ![](http://i.epvpimg.com/sc20bab.png) ![](http://i.epvpimg.com/mduSeab.png) ![](https://i.epvpimg.com/Ej2ceab.png) ![](http://i.epvpimg.com/Ixvvfab.png) ![](http://i.epvpimg.com/j1Ycgab.png)
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -13,7 +13,7 @@ import cv from "../../cv.pdf";
 // import { useSelector, useDispatch } from "react-redux";
 // import { increment, decrement, logged } from "../../actions";
 
-class About extends React.Component {
+class Home extends React.Component {
   // const counter = useSelector(state => state.counter);
   // const isLogged = useSelector(state => state.isLogged);
   // const dispatch = useDispatch();
@@ -194,6 +194,7 @@ class About extends React.Component {
             className="btn btn-danger"
           >
             Download resume
+            <i className="fas fa-angle-down"></i>
           </a>
           <button
             className="intro-scroll"
@@ -398,87 +399,6 @@ class About extends React.Component {
               >
                 Why do you want to leave your current job ?
               </button>
-              {/*<div className="answer1">
-                  <p>
-                    If I were to describe myself in one word that would be <b />
-                    self-taught
-                    <b />. Examples: Coding (Software & Web Development), Adobe
-                    Creative Suite (Video & Photo editing), Guitar.
-                    <br />
-                    <br />
-                    Ever since my younger days I was a curious individual. After
-                    playing for a few days with any of the toys my parents
-                    bought me, it always ended by opening them up to find out
-                    what are the mechanisms that make them work the way they do.
-                    <br />
-                    <br />
-                    At 10 years old I builded my first computer piece by piece.
-                    <br />
-                    And more recently I managed to 'hack(intosh)' my AMD Ryzen
-                    computer into running macOS (Apple's OS), running both
-                    Windows 10 and macOS High Sierra in dual-boot.
-                  </p>
-                </div>
-                <button type="button" className="collapse">
-                  What are your biggest weaknesses ?
-                </button>
-                <div className="answer2">
-                  <p>
-                    One that comes to mind right away is getting so absorbed in
-                    my work that I lose track of time. I know I should be more
-                    aware of the clock, but when I love what I'm doing I just
-                    can't think of anything else.
-                    <br />
-                    <br />
-                    Another one would be the fact that I'm brutally honest with
-                    people around me. Some people are finding this hard to
-                    swallow but this is my way of making sure that I don't waste
-                    anybody's time. Going straight to the problem's core and
-                    fixing it right away.
-                  </p>
-                </div>
-                <button type="button" className="collapse">
-                  Tell me about the toughest decision you had to make in the
-                  past years.
-                </button>
-                <div className="answer3">
-                  <p>
-                    The story goes such that about two years ago, working a
-                    factory job in Romania I realised that there's no future for
-                    me there. I felt stuck in my own country with no options.
-                    <br />
-                    <br />
-                    Therefore I choose to look forward to a decision I wanted to
-                    make for a long time. I sold my computer and my guitar,
-                    booked my plane ticket and I flew to England.
-                    <br />
-                    <br />
-                    It was the thoughest decision in my life to leave home with
-                    £50 cash in my wallet and a plane ticket but it was worth it
-                    every penny because now I can honestly say that it was the
-                    best decision I ever made.
-                  </p>
-                </div>
-                <button type="button" className="collapse">
-                  Why do you want to leave your current job ?
-                </button>
-                <div className="answer4">
-                  <p>
-                    Don't get me wrong, my current job as a Chef is a great
-                    artistic escape and I really enjoy cooking because is a
-                    primal thing we all share. I've had this job for 1.5 years
-                    now, it's not a full-time job and the only reason I've had
-                    theis job is to pay the bills while I was learning Web
-                    Development.
-                    <br />
-                    <br />I feel I have to move forward in my career progression
-                    as a Front-end Web Developer which I've been teaching myself
-                    for some time now and I am ready to go for it, whatever it
-                    takes.
-                  </p>
-                </div>
-              </div>
-              */}
             </div>
             <div className="main-footer">
               <a
@@ -514,4 +434,4 @@ class About extends React.Component {
   }
 }
 
-export default About;
+export default Home;
