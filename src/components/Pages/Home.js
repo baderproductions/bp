4cbebbf636cdf41201dd981c794f22ce84c7f970
baderproductions <kevin.bader96@gmail.com>
2020-03-12 19:31:02 +0000

    done, just finished with media querries 320px to 4k

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,9 +5,6 @@ import profilePic from "../../profile.jpg";
 import lsLogo from "../../lslogo.png";
 import spleeterLogo from "../../spleeter.png";
 import bg from "../../bg.jpg";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
 
 // import { useSelector, useDispatch } from "react-redux";
 // import { increment, decrement, logged } from "../../actions";
@@ -19,41 +16,142 @@ class About extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      open: false,
+      p1: "",
+      p2: "",
+      p3: "",
+      p4: "",
+      t1: "",
+      t2: "",
+      t3: "",
+      t4: ""
     };
-
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
 
     this.introRef = React.createRef();
     this.projectsRef = React.createRef();
     this.aboutRef = React.createRef();
   }
 
-  handleOpenModal() {
-    this.setState({ showModal: true });
+  componentWillMount() {
+    document.addEventListener("mousedown", this.closeBind, false);
   }
 
-  handleCloseModal() {
-    this.setState({ showModal: false });
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.closeBind, false);
+  }
+
+  handleBind() {
+    this.setState({ open: true });
+  }
+
+  closeBind = e => {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+    this.setState({ open: false });
+  };
+
+  p1State() {
+    this.setState({
+      t1: "Tell me a little about yourself",
+      p1: `If I were to describe myself in one word that would be
+      <b>self-taught</b>. Examples: coding (software & web development),
+      Adobe Creative Suite (video & photo editing), guitar.
+      <br />
+      <br />
+      Ever since my younger days I was a curious individual. After
+      playing for a few days with any of the toys my parents bought for
+      me, it always ended by opening them up to find out what are the
+      mechanisms that make them work the way they do.
+      <br />
+      <br />
+      At 10 years old I builded my first computer piece by piece. And
+      more recently I managed to 'hack(intosh)' my AMD Ryzen computer
+      into running both macOS (Apple's OS) and Windows 10 in dual-boot.`,
+      t2: "",
+      t3: "",
+      t4: "",
+      p2: "",
+      p3: "",
+      p4: ""
+    });
+  }
+
+  p2State() {
+    this.setState({
+      t2: "What are your biggest weaknesses ?",
+      p2: `One that comes to mind right away is getting so absorbed in
+      my work that I lose track of time. I know I should be more
+      aware of the clock, but when I love what I'm doing I just
+      can't think of anything else.
+      <br />
+      <br />
+      Another one would be the fact that I'm brutally honest with
+      people around me. Some people are finding this hard to
+      swallow but this is my way of making sure that I don't waste
+      anybody's time. Going straight to the problem's core and
+      fixing it right away.`,
+      t1: "",
+      t3: "",
+      t4: "",
+      p1: "",
+      p3: "",
+      p4: ""
+    });
+  }
+
+  p3State() {
+    this.setState({
+      t3: "What is the toughest decision you had to make ?",
+      p3: `The story goes such that about two years ago, working a
+      factory job in Romania I realised that there's no future for
+      me there. I felt stuck in my own country with no options.
+      <br />
+      <br />
+      Therefore I choose to look forward to a decision I wanted to
+      make for a long time. I sold my computer and my guitar,
+      booked my plane ticket and I flew to England.
+      <br />
+      <br />
+      It was the thoughest decision in my life to leave home with
+      £50 cash in my wallet and a plane ticket but it was worth it
+      every penny because now I can honestly say that it was the
+      best decision I made so far.`,
+      t1: "",
+      t2: "",
+      t4: "",
+      p1: "",
+      p2: "",
+      p4: ""
+    });
+  }
+
+  p4State() {
+    this.setState({
+      t4: "Why do you want to leave your current job ?",
+      p4: `My current part-time job as a waiter is just to cover the rent and other daily expenses while learning Web Development.
+      I've been learning Web Development and working part-time ever since I came to England 1.5 years ago and I'm ready to jump full-time into the career that I always wanted and I worked so hard to learn.
+      <br />
+      <br />I feel I have to move forward in my career progression
+      as a Front-end Web Developer which I've been teaching myself
+      for some time now and I am ready to go for it, whatever it
+      takes.`,
+      t1: "",
+      t2: "",
+      t3: "",
+      p1: "",
+      p2: "",
+      p3: ""
+    });
   }
 
   render() {
-    const page_title = "BADERproductions - Home";
+    const page_title = "BADERproductions - Frontend Web Developer, London";
 
-    const customStyles = {
-      content: {
-        height: "80%",
-        width: "80%",
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        transform: "translate(-50%, -50%)",
-        borderRadius: "2px"
-      },
-      overlay: {
-        backgroundColor: "transparent"
+    const st = {
+      def: {},
+      soHide: {
+        display: this.state.open ? "block" : "none"
       }
     };
 
@@ -62,21 +160,45 @@ class About extends React.Component {
         <Helmet>
           <title>{page_title}</title>
         </Helmet>
+        <div style={{ ...st.def, ...st.soHide }} className="about-modal">
+          <h3>
+            {this.state.t1 || this.state.t2 || this.state.t3 || this.state.t4}
+          </h3>
+          <div ref={node => (this.node = node)} className="inner-modal">
+            <p
+              dangerouslySetInnerHTML={{
+                __html:
+                  this.state.p1 ||
+                  this.state.p2 ||
+                  this.state.p3 ||
+                  this.state.p4
+              }}
+            />
+          </div>
+          <p id="close-touch">Tap outside to close</p>
+        </div>
         <div ref={this.introRef} className="about-page">
           <button
             className="intro-scroll"
             onClick={() => window.scrollTo(0, this.introRef.current.offsetTop)}
-          ></button>
+          >
+            <i className="fas fa-circle"></i>
+          </button>
           <button
             className="project-scroll"
             onClick={() =>
               window.scrollTo(0, this.projectsRef.current.offsetTop)
             }
-          ></button>
+          >
+            <i className="fas fa-angle-up"></i>
+            <i className="fas fa-angle-down"></i>
+          </button>
           <button
             className="about-scroll"
             onClick={() => window.scrollTo(0, this.aboutRef.current.offsetTop)}
-          ></button>
+          >
+            <i className="fas fa-angle-double-down"></i>
+          </button>
           <div className="darken-layer"></div>
           <img src={bg} alt="Optic Fiber Background Art" />
           <div className="intro">
@@ -92,29 +214,28 @@ class About extends React.Component {
             <section className="project-section">
               <div className="card">
                 <img
-                  onClick={this.handleOpenModal}
                   src={lsLogo}
                   id="lsp"
                   className="card-img-top"
                   alt="Project Display"
                 />
-                <Modal
-                  style={customStyles}
-                  isOpen={this.state.showModal}
-                  contentLabel="onRequestClose Example"
-                  onRequestClose={this.handleCloseModal}
-                >
-                  <p>Modal text!</p>
-                  <button onClick={this.handleCloseModal}>Close Modal</button>
-                </Modal>
                 <div className="card-body">
                   <h5 className="card-title">Lazăr Sergiu Photographer</h5>
+                  <img
+                    src={lsLogo}
+                    id="lsp-body"
+                    className="card-img-top"
+                    alt="Project Display"
+                  />
                   <p className="card-text">
                     React based website builded for a hobbyist photographer.
                     Logo design included.
                   </p>
                 </div>
-                <ul className="list-group list-group-flush"></ul>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item"></li>
+                  <li className="list-group-item">React, Frontend</li>
+                </ul>
                 <div className="card-body">
                   <a
                     className="card-link"
@@ -135,29 +256,29 @@ class About extends React.Component {
               </div>
               <div className="card">
                 <img
-                  onClick={this.handleOpenModal}
                   src={spleeterLogo}
                   id="spleeter"
                   className="card-img-top"
                   alt="Project Display"
                 />
-                <Modal
-                  style={customStyles}
-                  isOpen={this.state.showModal}
-                  contentLabel="onRequestClose Example"
-                  onRequestClose={this.handleCloseModal}
-                >
-                  <p>Modal text!</p>
-                  <button onClick={this.handleCloseModal}>Close Modal</button>
-                </Modal>
+
                 <div className="card-body">
-                  <h5 className="card-title">Django Spleeter by Deezer</h5>
+                  <h5 className="card-title">Spleeter by Deezer</h5>
+                  <img
+                    src={spleeterLogo}
+                    id="spleeter-body"
+                    className="card-img-top"
+                    alt="Project Display"
+                  />
                   <p className="card-text">
                     Spleeter by Deezer uses Tensorflow to separate vocals and
                     accompaniment from a song.
                   </p>
                 </div>
-                <ul className="list-group list-group-flush"></ul>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item"></li>
+                  <li className="list-group-item">Django, Backend</li>
+                </ul>
                 <div className="card-body">
                   <a
                     className="card-link"
@@ -218,12 +339,49 @@ class About extends React.Component {
             </div>
             <div className="questions">
               <i className="far fa-question-circle"></i>
-              {/*
-              <div className="buttons">
-                <button type="button" className="collapse">
-                  Tell me a little about yourself.
-                </button>
-                <div className="answer1">
+              <button
+                onClick={() => {
+                  this.handleBind();
+                  this.p1State();
+                }}
+                type="button"
+                className="b1"
+              >
+                Tell me a little about yourself
+              </button>
+
+              <button
+                onClick={() => {
+                  this.handleBind();
+                  this.p2State();
+                }}
+                type="button"
+                className="b2"
+              >
+                What are your biggest weaknesses ?
+              </button>
+
+              <button
+                onClick={() => {
+                  this.handleBind();
+                  this.p3State();
+                }}
+                type="button"
+                className="b3"
+              >
+                What is the toughest decision you had to make ?
+              </button>
+              <button
+                onClick={() => {
+                  this.handleBind();
+                  this.p4State();
+                }}
+                type="button"
+                className="b4"
+              >
+                Why do you want to leave your current job ?
+              </button>
+              {/*<div className="answer1">
                   <p>
                     If I were to describe myself in one word that would be <b />
                     self-taught
@@ -305,26 +463,26 @@ class About extends React.Component {
               </div>
               */}
             </div>
-          </div>
-          <div className="main-footer">
-            <a
-              href="https://github.com/baderproductions"
-              title="GitHub Account"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              href="https://www.instagram.com/baderproductions/"
-              title="Instagram Account"
-            >
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/kevin-bader-2137541a2/"
-              title="LinkedIn Account"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
+            <div className="main-footer">
+              <a
+                href="https://github.com/baderproductions"
+                title="GitHub Account"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+              <a
+                href="https://www.instagram.com/baderproductions/"
+                title="Instagram Account"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kevin-bader-2137541a2/"
+                title="LinkedIn Account"
+              >
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
           </div>
           {/*
         <h1>Counter: {counter}</h1>
--- src/components/Pages/NotFound.js
+++ src/components/Pages/NotFound.js
@@ -1,17 +1,23 @@
 import React from "react";
-import logoBP from '../../logo.png';
+import logoBP from "../../logo.png";
+import Helmet from "react-helmet";
 
 class NotFound extends React.Component {
-
-    render() {
-        return (
-            <div className="notfound-container">
-                <img src={logoBP} alt="BADERproductions Logo"/>
-                <h1>Page not found</h1>
-                <p>The page you just tried to access was not found.</p>
-            </div>
-        );
-    }
+  render() {
+    const page_title = "BADERproductions - Page not found";
+    return (
+      <>
+        <Helmet>
+          <title>{page_title}</title>
+        </Helmet>
+        <div className="notfound-container">
+          <img src={logoBP} alt="BADERproductions Logo" />
+          <h1>Page not found</h1>
+          <p>The page you tried to access was not found</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default NotFound;
