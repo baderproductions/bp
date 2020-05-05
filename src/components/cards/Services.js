b00f3cd1660e420e0ed4c913669a509e8f077b2b
BADERproductions <56457735+baderproductions@users.noreply.github.com>
2020-05-05 21:13:07 +0100

    rem digocean

--- src/components/cards/Services.js
+++ src/components/cards/Services.js
@@ -16,26 +16,16 @@ class Services extends Component {
             <a href="https://cloud.google.com/" className="badge badge-danger">
               Google Cloud Computing
             </a>
-
             <a href="https://azure.microsoft.com/" className="badge badge-dark">
               Microsoft Azure
             </a>
-
             <a href="https://aws.amazon.com/" className="badge badge-warning">
               Amazon Web Services
-            </a>
-
-            <a
-              href="https://www.digitalocean.com/"
-              className="badge badge-primary"
-            >
-              DigitalOcean
             </a>
             <p id="stack2">{t("services.sub2")}</p>
             <a href="https://www.nginx.com/" className="badge badge-success">
               NGINX
             </a>
-
             <a
               href="https://www.apache.org/"
               style={{ backgroundColor: "#282661" }}
@@ -43,11 +33,9 @@ class Services extends Component {
             >
               Apache
             </a>
-
             <a href="http://supervisord.org/" className="badge badge-primary">
               Supervisor
             </a>
-
             <a
               href="https://gunicorn.org/"
               style={{ backgroundColor: "#1D692D" }}
