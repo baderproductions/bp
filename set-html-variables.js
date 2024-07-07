773be46e3b258eaf59b72b082c3bbe6585c01be5
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-07-07 15:16:36 -0700

    added prebuild script command directly in build

--- set-html-variables.js
+++ set-html-variables.js
@@ -3,13 +3,9 @@ const fs = require('fs');
 require('dotenv').config();
 
 const indexPath = path.join('', 'index.html');
-console.log('### indexPath', indexPath);
 const VITE_APP_CDN = process.env.VITE_APP_CDN;
 const VITE_APP_OG_URL = process.env.VITE_APP_OG_URL;
 const VITE_APP_OG_TWITTER_HANDLE = process.env.VITE_APP_OG_TWITTER_HANDLE;
-console.log('# VITE_APP_CDN', VITE_APP_CDN);
-console.log('# VITE_APP_OG_URL', VITE_APP_OG_URL);
-console.log('# VITE_APP_OG_TWITTER_HANDLE', VITE_APP_OG_TWITTER_HANDLE);
 
 fs.readFile(indexPath, 'utf8', (err, data) => {
     if (err) return console.log(err);
